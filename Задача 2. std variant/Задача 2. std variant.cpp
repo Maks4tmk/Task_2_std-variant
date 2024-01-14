9f717a64bbe8e687c5160e9e055dbf8daa5d5a52
Maksim <maks4tmk5423@yandex.ru>
2024-01-14 20:52:42 +0300

    initial_commit

---
+++ Задача 2. std variant/Задача 2. std variant.cpp
@@ -0,0 +1,54 @@
+﻿// Задача 2. std variant.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <variant>
+#include <vector>
+#include <ctime>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+    std::srand(std::time(nullptr));
+    int random_variable = std::rand() % 3;
+
+    std::variant<int, std::string, std::vector<int>> result;
+    switch (random_variable)
+    {
+    case 0:
+        result = 5;
+        break;
+    case 1:
+        result = "string";
+        break;
+    case 2:
+        result = std::vector<int>{ 1, 2, 3, 4, 5 };
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+int main()
+{
+    auto tmp = get_variant();
+
+    if (std::holds_alternative<int>(tmp)) {
+        auto tru_int = std::get_if<int>(&tmp);
+        std::cout << *tru_int * 2;
+    }
+
+    if (std::holds_alternative<std::string>(tmp)) {
+        auto tru_string = std::get_if<std::string>(&tmp);
+        std::cout << *tru_string;
+    }
+
+    if (std::holds_alternative<std::vector<int>>(tmp)) {
+        auto tru_vector = std::get_if<std::vector<int>>(&tmp);
+        for (int i : *tru_vector) {
+            std::cout << i << " ";
+        }
+    }
+
+}
+
